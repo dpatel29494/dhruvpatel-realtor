154593e271b7fead3e3d4a490d4d866579fe4b30
Dhruv Patel <dpatel29494@gmail.com>
2026-05-08 09:12:11 -0600

    Import Checklist component in App.tsx

--- src/App.tsx
+++ src/App.tsx
@@ -6,6 +6,7 @@ import { QueryClient, QueryClientProvider } from "@tanstack/react-query";
 import { BrowserRouter, Routes, Route } from "react-router-dom";
 import Index from "./pages/Index";
 import NotFound from "./pages/NotFound";
+import Checklist from "./components/checklist";
 
 const queryClient = new QueryClient();
 
